dce95d60356c5cb9a9539f22dcb303324bffaadd
Shakya, Jebin <jshakya@gatech.edu>
2022-04-06 22:20:40 -0500

    Corrected Location for TaxJarCalculation

--- Sources/Calculators/TaxjarCalculator.cs
+++
@@ -1,7 +0,0 @@
-namespace WebApiClient
-{
-    class TaxjarCalculator : ICalculator
-    {
-        
-    }
-}
---
+++ Sources/WebAPIClient/Calculators/TaxjarCalculator.cs
@@ -0,0 +1,15 @@
+namespace WebApiClient
+{
+    class TaxjarCalculator : ICalculator
+    {
+        public void CalculateSalesTaxForOrder(string to_country, float shipping)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetTaxRate(string zip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
